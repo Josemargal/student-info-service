1d984fe2e694354f4b04ffcfa91a8e2d4ba28b98
Josemargal <126793937+Josemargal@users.noreply.github.com>
2025-05-21 16:42:56 +0200

    sol lab

--- src/main/java/com/example/demo/StudentInfoServiceApplication.java
+++ src/main/java/com/example/demo/StudentInfoServiceApplication.java
@@ -1,14 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class StudentInfoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentInfoServiceApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner init(StudentRepository studentRepository) {
+        return args -> {
+           studentRepository.save(new Student(null,"Juan Pérez", 20));
+            studentRepository.save(new Student(null,"Ana Gómez", 22));
+            studentRepository.save(new Student(null,"Carlos Ruiz", 19));
+       };
+   }
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentInfoServiceApplication.class, args);
-	}
 }
--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,23 +1,24 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long studentId;
+    private String studentName;
+    private int studentAge;
 
-    private String name;
-    private int age;
 }
